16de7e5cdc12e0462fc07580de7631706bc6c4d9
Vanelle Backend <backendvanelle@gmail.com>
2025-05-12 23:50:41 +0200

    il me manque seulement implémenter le module pour la suprression des taches

--- Pouakam/Boo-Mind/index.php
+++ Pouakam/Boo-Mind/index.php
@@ -2,38 +2,40 @@
     session_start();
     //inclusion du module relative au stockage des taches ajouter dans le fichier datBase.json...
     require_once('taskStorage.php');
+    // Initialiser $_SESSION['taches'] s'il n'existe pas déjà
+    if (!isset($_SESSION['taches'])) {
+        $_SESSION['taches'] = [];
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BooMind</title>
-    <link rel="stylesheet" href="style-project/style.css">
+    <title>BooMind.cm</title>
+    <link rel="stylesheet" href="style_Project/style.css">
+    <link rel="stylesheet" href="style_Project/bootstrap-icons/bootstrap-icons.css">
 </head>
 <body>
     <div class="container">
         <h1>BooMind</h1>
         <!-- Formulaire pour add les tasks... -->
-        <form action="#" method="post" class="input-container">
-            <input type="text" name="taskText" id="taskInput" placeholder="Enter your Task..." autocomplete="off" required>
-            <button type="submit" name="addTaskBtn" id="addTaskBtn">Add Task</button>
-        </form>
-    </div><br>
-    <div class="container-view-task">
-        <?php
-            // affichage des taches...
-            if(isset($_SESSION['taches']) && !empty($_SESSION['taches'])){
-                echo '<h2>My Tasks</h2>';
-                echo '<ul class="task-list">';
-                foreach($_SESSION['taches'] as $indexTask => $task){
-                    echo '<li class="task-item">'.$task.'</li>';
+        <form action="#" method="post">
+            <div class="input-container">
+                <input type="text" name="taskText" id="taskInput" placeholder="Enter your Task..." autocomplete="off" required>
+                <button type="submit" name="addTaskBtn" id="addTaskBtn">Add Task</button>
+            </div>
+            <?php
+                foreach($_SESSION['taches'] as $keysTasks => $lisTasks){
+                    echo '<div class="container-view-task">';
+                        echo '<span>'.$lisTasks.'</span>';            
+                        echo '<button type="submit" name="deleteTaskBtn" class="deleteTaskBtn">
+                                <i class="bi bi-trash"></i>
+                            </button>';
+                    echo '</div>';
                 }
-                echo '</ul>';
-            } else {
-                echo '<h2>No Tasks Available...</h2>';
-            }
-        ?>
+            ?>
+        </form>
     </div>
 </body>
 </html>
--- Pouakam/Boo-Mind/taskStorage.php
+++ Pouakam/Boo-Mind/taskStorage.php
@@ -12,9 +12,16 @@
         $tasks = json_encode($tasks, JSON_PRETTY_PRINT);
         file_put_contents('dataBase.json', $tasks);
     }
+
+    // initilisation de la variable $_SESSION['taches']...
+    if(file_exists('dataBase.json')) {
+        $_SESSION['taches'] = json_decode(file_get_contents('dataBase.json'), true);
+    } else{
+        $_SESSION['taches'] = array();
+    }
     
     // filtrage du champs de texte de la tache et ajout des taches dans ma base de donnée (dataBase.json)...
-    if(isset($_POST['addTaskBtn'])){
+    if(isset($_POST['addTaskBtn'])) {
         if(isset($_POST['taskText']) && !empty($_POST['taskText'])){
             $_SESSION['taches'][] = filterTasks($_POST['taskText']);
             // stockage des taches dans le fichier dataBase.json...
