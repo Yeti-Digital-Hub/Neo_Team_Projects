a2b3a40cf13953c8f924d1a4a7dc474beb24c4f8
Kelie-K <comptegithubetcodes@gmail.com>
2025-05-13 17:44:11 +0100

    ajout des citations avec clicks

--- Kelie/citationdynamique/README.md
+++ Kelie/citationdynamique/README.md
@@ -1,1 +1,2 @@
 Bonjour tous!
+Après des jours sur une multitude d'essai sur mon défis, qui au passage a été fustrant du fait des échecs mais un ingenieur doit avoir un mindset élévé,
---
+++ Kelie/citationdynamique/script.js
@@ -0,0 +1,49 @@
+
+const Quotes =[
+     {
+        quote:"The pessimist sees difficulty in every opportunity.",
+        author: "__Robert Greene"
+    },
+    {
+    quote: "The pessimist sees difficulty in every opportunity.The optimist sees the opportunity in every difficult!",
+    author: "__Winston Churchill"
+    },
+    {
+    quote:  "La Connaissace est le Pouvoir!",
+    author:  "__Francis Bacon"
+    },
+    {
+    quote: "Le voyage de mille lieues commence par un pas!",
+    author:"__Lao Tseu"
+},
+  {
+    quote: "Le succès n'est pas final, l'échec n'est pas fatal: C'est le courage de continuer qui compte.",
+    author:"__Winston Churchill"
+  }, 
+  {
+    quote:"La seule façon de faire du bon travail est d'aimer ce que vous faites.",
+    author:"__Steve Jobs"
+  },
+  {
+    quote:  "Il y a qu'une erreur innée, c'est celle qui consiste de croire que nous existons pour être heureux.",
+    author:"__Arthur Schopenhower"
+  },
+  {
+    quote: "L'imagination est plus importante que le savoir." ,
+    author: "__Albert Einstein"
+  },
+  {
+    quote: "Courir et se gratter les pieds ne vont pas ensemnle!",
+    author: "__#Amassoka"
+  },
+    
+    ];
+    
+function newQuote(){
+    const randomIndex=Math.floor(Math.random()*Quotes.length);
+    const quote = document.getElementById('quote');
+    const author = document.getElementById('author');
+    quote.innerText = `${Quotes[randomIndex].quote}`;
+    author.innerText = `-${Quotes[randomIndex].author}`;
+}
+    
